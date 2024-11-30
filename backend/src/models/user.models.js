101163a3dc1ec6943155b10def84a6e23bb33718
mohammdhusen zhare <mohammadhusenzhare@gmail.com>
2024-11-30 13:40:31 +0530

    generate webtokens

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -30,4 +31,49 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.methods.genereateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_SECRET_TOKEN,
+    {
+      expiresIn: process.env.ACCESS_Token_EXP,
+    }
+  );
+};
+
+userSchema.methods.genereateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_SECRET_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_Token_EXP,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
+
+/*
+1. Web Token 
+  - a digital key for confirmation
+  - commonly used in web applications 
+  - securely share information in between
+
+2. Access Token
+  - let a user access specific parts of application
+  - when you login the server provide you with an access token
+  - short lived (15- 30mins) for security
+
+3. Refresh Tokens
+  - Get a new access token without logging in again
+  - access token expires, then send refresh token to the server to get a new access token
+  - Long-lived (hours, days).
+
+JWT (Josn Web Token) --> used for user authentication and authorization
+*/
